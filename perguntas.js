8c9532afbf32c177ade5c7590ab3398b2c25b00e
anna <141247114+emyssxz@users.noreply.github.com>
2024-11-20 19:32:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlin'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma condicionais?',
+    'Condicionais são usados para tomar decisões em um programa com base em determinadas condições.'
+)
+
+criaCartao(
+    'Lingua alemã',
+    'Como se diz boa noite em alemão?',
+    'Boa noite em alemão é Gute Nacht'
+)
